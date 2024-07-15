3c6d5bdc10bd1162fbee9f3f1083d0e587e3da19
unknown <lizosazi@gmail.com>
2024-07-15 22:06:23 +0200

    connected contentful cms to my site

---
+++ src/client.js
@@ -0,0 +1,6 @@
+import * as contentful from "contentful";
+
+export const client = contentful.createClient({
+    space: "f6h9sdw34o4o",
+    accessToken:"wrfSg_p9uiMbmlIb-EMuVmW1eIHYbnwkA4Xhmuuuzd8",
+});
